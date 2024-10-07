a7726f75bfa410f2f5323898b6da02162b79a27e
Rynx007 <rynx007@gmail.com>
2024-10-08 00:22:09 +0545

    creating the heroContent

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
+import Hero from "@/components/main/Hero";
 
 export default function Home() {
   return (
     <main className="h-full w-fll">
-      <div className="flex flex-col gap-20">
-        Hi
-        </div>
+      <div className="flex flex-col h-[850px] gap-20">
+        <Hero />
+      </div>
     </main>
   );
 }
