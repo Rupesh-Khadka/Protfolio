52c4ee11bdebff7418a046a00c90c5b093da5409
Rynx007 <rynx007@gmail.com>
2024-10-09 22:36:05 +0545

    Completed the project section

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Encryption from "@/components/main/Encryption";
 import Hero from "@/components/main/Hero";
+import Projects from "@/components/main/Projects";
 import Skills from "@/components/main/Skills";
 
 export default function Home() {
@@ -8,8 +9,8 @@ export default function Home() {
       <div className="flex flex-col gap-20">
         <Hero />
         <Skills />
-      <Encryption />
-
+        <Encryption />
+        <Projects />
       </div>
     </main>
   );
--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import ProjectCard from "../sub/ProjectCard";
 
 const Projects = () => {
   return (
-    <div>Projects</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center py-20 ">
+      <h1 className="text-[40px] font-semibold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500 py-20">
+        My Projects
+      </h1>
+      <div className="h-full w-full flex flex-col md:flex-row gap-10 px-10">
+        <ProjectCard
+          src="/NextWebsite.png"
+          title="Food Delivery app"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, purus eu venenatis sagittis, nisi leo ullamcorper eros, vitae scelerisque nisl lectus non erat."
+        />
+        <ProjectCard
+          src="/CardImage.png"
+          title="Expense Tracker"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, purus eu venenatis sagittis, nisi leo ullamcorper eros, vitae scelerisque nisl lectus non erat."
+        />
+        <ProjectCard
+          src="/SpaceWebsite.png"
+          title="Modern NextJs portfolio"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus luctus urna sed urna ultricies ac tempor dui sagittis. In condimentum facilisis porta. Sed nec diam eu diam mattis viverra. Nulla fringilla, purus eu venenatis sagittis, nisi leo ullamcorper eros, vitae scelerisque nisl lectus non erat."
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Projects
+export default Projects;
