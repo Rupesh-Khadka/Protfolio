f57422a5d8fe9d69b9a28935319354e17b2a6733
Rynx007 <rynx007@gmail.com>
2024-10-09 23:01:05 +0545

    completed footer aswell

--- components/main/Footer.tsx
+++ components/main/Footer.tsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import React from "react";
+import { FaInstagram, FaLinkedin, FaTwitter, FaYoutube } from "react-icons/fa";
+import { RxGithubLogo, RxLinkedinLogo } from "react-icons/rx";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="w-full h-full bg-transparent text-gray-200 shadow-lg p-[15px] ">
+      <div className="w-full flex flex-col items-center justify-center m-auto ">
+        <div className="w-full h-full flex flex-row items-center justify-around flex-wrap">
 
-export default Footer
+
+          <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
+            <div className="font-bold text-[16px] ">Community</div>
+            <p className="flex flex-row items-center my-[15px] cursor-pointer">
+              <FaYoutube />
+              <span className="text-[15px] ml-[6px]">Youtube</span>
+            </p>
+            <p className="flex flex-row items-center my-[15px] cursor-pointer">
+              <RxGithubLogo />
+              <span className="text-[15px] ml-[6px]">Github</span>
+            </p>
+            <p className="flex flex-row items-center my-[15px] cursor-pointer">
+              <RxLinkedinLogo />
+              <span className="text-[15px] ml-[6px]">LinkedIn</span>
+            </p>
+          </div>
+          
+          
+          <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
+            <div className="font-bold text-[16px] ">Social Media</div>
+            <p className="flex flex-row items-center my-[15px] cursor-pointer">
+              <FaInstagram />
+              <span className="text-[15px] ml-[6px]">Instagram</span>
+            </p>
+            <p className="flex flex-row items-center my-[15px] cursor-pointer">
+              <FaTwitter />
+              <span className="text-[15px] ml-[6px]">Twitter</span>
+            </p>
+            <p className="flex flex-row items-center my-[15px] cursor-pointer">
+              <FaLinkedin />
+              <span className="text-[15px] ml-[6px]">LinkedIn</span>
+            </p>
+          </div>
+          
+          
+          <div className="min-w-[200px] h-auto flex flex-col items-center justify-start">
+            <div className="font-bold text-[16px] ">About</div>
+            <p className="flex flex-row items-center my-[15px] cursor-pointer">
+              <span className="text-[15px] ml-[6px]">Become Sponsor</span>
+            </p>
+            <p className="flex flex-row items-center my-[15px] cursor-pointer">
+              <span className="text-[15px] ml-[6px]">Learning about me</span>
+            </p>
+            <p className="flex flex-row items-center my-[15px] cursor-pointer">
+              <span className="text-[15px] ml-[6px]">rupeshkhadka000@gmail.com</span>
+            </p>
+          </div>
+          
+        </div>
+
+        <div className='mb-[20px] text-[15px] text-center'>
+          &copy; Rupesh Khadka 2024 Inc. All rights reserved 
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
